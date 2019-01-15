11d61760c890581c78fcbc2c1e6673c18bcfe098
liangyaoyi <943206815@qq.com>
2019-01-15 11:37:50 +0800

    phone

---
+++ Phone.java
@@ -0,0 +1,16 @@
+package 小实验;
+
+public class Phone {
+	float sceet;
+	float cpu;
+	float mem;
+	void call() {
+		System.out.println("phone有发短信的功能");
+		
+	}
+	void sendMessage() {
+		System .out .println("sceet:"+sceet+"cpu:"+cpu+"mem:"+mem+"phone具有发短信的功能");
+		
+	}
+
+}
---
+++ Telphone.java
@@ -0,0 +1,13 @@
+package 小实验;
+
+public class Telphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Telphone phone = new Telphone();
+		phone.sendMessage;
+		System.out .println("phone.sendMessage");
+
+	}
+
+}
